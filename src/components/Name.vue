9f79c3da6d008d41fe2215383442d113f7b8774d
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2020-08-02 23:26:35 +0900

    slotの使い方テスト

---
+++ src/components/Name.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>name:<slot name="name">Mirai Taro</slot></p>
+    <p>address:<slot name="adrs">Osaka</slot></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NameComponent",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <NameComponent v-slot:name>Name Slot Test</NameComponent>
+    <NameComponent v-slot:adrs>Tokyo</NameComponent>
   </div>
 </template>
+
+<script>
+import NameComponent from "@/components/Name.vue";
+
+export default {
+  components: {
+    NameComponent,
+  },
+};
+</script>
