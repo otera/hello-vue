bb26563ea27f9ef0db689ff1ebea9216960d8021
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2020-08-03 00:10:00 +0900

    ドラッグ終了後にイベント実行するように変更

--- src/views/Drag.vue
+++ src/views/Drag.vue
@@ -2,22 +2,34 @@
   <div class="row">
     <div class="col-8">
       <h3>Draggable table</h3>
-
       <table class="table table-striped">
         <thead class="thead-dark">
-          <draggable
-            v-model="headers"
-            :options="{ handle: '.item-handle' }"
-            tag="tr"
-          >
-            <th v-for="header in headers" :key="header.key" scope="col">
-              <span class="item-handle">::</span>
-              {{ header.name }}
+          <tr>
+            <th rowspan="2">Check</th>
+            <draggable
+              v-model="headers"
+              :options="{ handle: '.item-handle' }"
+              @end="draggableEnd"
+            >
+              <th v-for="header in headers" :key="header.key" scope="col">
+                <span class="item-handle">::</span>
+                {{ header.name }}
+              </th>
+            </draggable>
+          </tr>
+          <tr>
+            <th v-for="header2 in headersSecond" :key="header2.key">
+              <input
+                type="text"
+                :name="header2.key"
+                :placeholder="header2.name"
+              />
             </th>
-          </draggable>
+          </tr>
         </thead>
         <tbody>
           <tr v-for="item in list" :key="item.name">
+            <td><input type="checkbox" /></td>
             <td v-for="header in headers" :key="header.key">
               {{ item[header.key] }}
             </td>
@@ -32,13 +44,26 @@
 import draggable from "vuedraggable";
 export default {
   name: "Drag",
-  methods: {},
+  methods: {
+    draggableEnd(event) {
+      // ドラッグ後のデータ順を確認
+      console.log(event);
+      this.headers.forEach((data, index) => {
+        console.log("index:" + index, data.key, data.name);
+      });
+    },
+  },
   components: {
     draggable,
   },
   data() {
     return {
       headers: [
+        { key: "id", name: "ID" },
+        { key: "name", name: "名前" },
+        { key: "sport", name: "スポーツ" },
+      ],
+      headersSecond: [
         { key: "id", name: "ID" },
         { key: "name", name: "名前" },
         { key: "sport", name: "スポーツ" },
